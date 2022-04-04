23fe486bed8558d66722bd6601ff56fbc7ddfaf5
pranab56 <pronabhalder53@gmail.com>
2022-04-04 20:08:53 +0600

    all file ready

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='fs-1 font-monospace'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
