2273be12a314a92627b069193963f87d2a1d981e
pranab56 <pronabhalder53@gmail.com>
2022-04-04 21:50:52 +0600

    About section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,12 +3,12 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1 className='text-center mt-5 text-primary'>Welcome to Mac Zone</h1>
+            <h1 className='text-center mt-5 text-primary'>Welcome to Honda Motor</h1>
             <p className='text-center text-secondary'>We are always ready to build your experience better.</p>
             <h5 className='text-center'>To know more about us follow us on social media</h5>
-            <h6 className='text-center text-primary'><i class="fa-brands fa-facebook"></i> Facebook/maczone</h6>
-            <h6 className='text-center text-danger'><i class="fa-brands fa-instagram"></i> Instagram/maczone</h6>
-            <h6 className='text-center text-info'><i class="fa-brands fa-twitter me-3"></i> Twitter/maczone</h6>
+            <h6 className='text-center text-primary'><i class="fa-brands fa-facebook"></i> Facebook/hondamotor</h6>
+            <h6 className='text-center text-danger'><i class="fa-brands fa-instagram"></i> Instagram/hondamotor</h6>
+            <h6 className='text-center text-info'><i class="fa-brands fa-twitter me-3"></i> Twitter/hondamotor</h6>
         </div>
     );
 };
