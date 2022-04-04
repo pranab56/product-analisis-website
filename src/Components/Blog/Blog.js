037c8a27e696d7a5fce93e42e0c77587783d7d96
pranab56 <pronabhalder53@gmail.com>
2022-04-04 22:27:08 +0600

    blogs component done

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
         <div className='container mt-5'>
-            <h4 className='text-center'>What is Context API?</h4>
+            <div className='question'>
+            <h4 className='text-center'># What is Context API?</h4>
             <p className='text-center mt-4'>The Context API is a React structure that enables you to exchange unique <br /> details and assists in solving prop-drilling from all levels of your application.</p>
-            <h4 className='text-center'>What is Semantic Tags?</h4>
+            </div>
+            <div className='question'>
+            <h4 className='text-center'># What is Semantic Tags?</h4>
             <p className='text-center mt-3'>Semantic HTML or semantic markup is HTML that introduces meaning to the <br /> web page rather than just presentation. For example, a p tag indicates that the <br /> enclosed text is a paragraph. This is both semantic and presentational because <br /> people know what paragraphs are, and browsers know how to display them.</p>
+            </div>
         </div>
     );
 };
